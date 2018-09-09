da14a1385fea3177078f407606fd50e8702561b7
Giacomo Sorbi <g.sorbi@gmail.com>
2018-09-09 23:31:07 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,11 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+  suite_a: "Amanda Presley", 
+  suite_b: "Seymour Hoffman", 
+  suite_c: "Alfred Tennyson", 
+  suite_d: "Charlie Chaplin", 
+  suite_e: "Crumpet the Elf"
+}
 
-def select_winner(passengers)
-  # add the code snippet here!
+def select_winner(p)
+  p.suite_a && p.suite_a[0] == 'A' ? p.suite_a ? ""
 end
